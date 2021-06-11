e169a3fe758ee6a6c81fc4acf88ee66df5764af5
LukeNK <64694123+LukeNk@users.noreply.github.com>
2021-06-11 23:25:17 +0700

    Fix txtEditor style

--- src/plugin/script/txtEditor.js
+++ src/plugin/script/txtEditor.js
@@ -8,12 +8,14 @@ PLUGINS.txtEditor = {
         document.getElementById('topBar').style.left = '0';
         document.getElementById('topBar').style.width = '100%';
         document.getElementById('navigationBar').style.visibility = 'hidden';
+        document.body.style.overflowY = 'hidden';
     },
     close: () => {
         document.getElementById('txtEditor').style.display = 'none';
         document.getElementById('topBar').style.left = '';
         document.getElementById('topBar').style.width = '';
         document.getElementById('navigationBar').style.visibility = '';
+        document.body.style.overflowY = '';
         activatingApp = '';
     }
 }
