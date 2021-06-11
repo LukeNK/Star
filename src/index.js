494653c77659cd388eae037fd7a284f33b782002
LukeNK <64694123+LukeNk@users.noreply.github.com>
2021-06-11 22:41:52 +0700

    Clean up for build Include `src` and `user` in build Adjust init process in main process

--- src/index.js
+++ src/index.js
@@ -22,147 +22,147 @@ app.whenReady().then(() => {
         }
     })
     win.loadFile('./index.html');
-    win.webContents.on('did-finish-load', windowReady)
+    win.webContents.on('did-finish-load', doneInit);
 })
 
-function windowReady() {
-    //all listener
-    //#region init
-    fs.readFile('./user/lastSessionPath', "utf8", (err, data) => {
-        if (err) {
-            fs.mkdirSync('./user');
-            fs.writeFileSync('./user/lastSessionPath', path.resolve('./'))
-            user.path = './'
-        } else user.path = data;
-        win.webContents.send('sendCurrentDir', path.resolve(user.path).replace(/\\/g, '/'));
-    });
+//all listener
+//#region init
+fs.readFile('./user/lastSessionPath', "utf8", (err, data) => {
+    if (err) {
+        fs.mkdirSync('./user');
+        fs.writeFileSync('./user/lastSessionPath', path.resolve('./'))
+        user.path = './'
+    } else user.path = data;
+});
 
-    //#endregion
-
-    //#region listener
-    ipcMain.on('getDirContent', (event, path) => {
-        listDirectory(path, (files, fType) => {
-            win.webContents.send('sendDirContent', files, fType)
-        })
-        user.path = path;
-        fs.writeFile('./user/lastSessionPath', user.path, (err) => {
-            if (err) console.log(err)
-        })
-    });
-
-    ipcMain.on('getFileContent', (event, file) => {
-        file = path.resolve(user.path, file); // file is relative
-        fs.readFile(file, 'utf8', (err, data) => {
-            if (err) {
-                win.webContents.send('sendFileContent', err);
-            } else
-                win.webContents.send('sendFileContent', data);
-        })
-    });
-
-    ipcMain.on('doSaveFile', (event, arr) => {
-        let file = arr[0];
-        let data = arr[1];
-        file = path.resolve(user.path, file); // file is relative
-        fs.writeFile(file, data, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-                win.webContents.send('didSaveFile', err);
-            }
-        });
-    });
-
-    ipcMain.on('doCopy', (event, files) => {
-        let callTime = files.length;
-        for (let file of files) {
-            let nDir = path.resolve(user.path, path.basename(file));
-            if (fs.lstatSync(file).isDirectory()) {
-                fs.ensureDirSync(nDir);
-                fs.copy(file, nDir, callCount);
-            } else {
-                fs.writeFile(nDir, '', (err) => {
-                    if (err) return;
-                    fs.copy(file, nDir, callCount)
-                })
-            }
-        }
-
-        function callCount(err) {
-            if (err) console.log(err);
-            callTime--;
-            if (callTime == 0)
-                listDirectory(user.path, (files, fType) => {
-                    win.webContents.send('sendDirContent', files, fType)
-                })
-        }
-    });
-
-    ipcMain.on('doMove', (event, files) => {
-        let a = user.path;
-        let callTime = files.length;
-        for (let file of files) {
-            let b = file.split(`/`);
-            b = b[b.length - 1];
-            fs.rename(file, a + ((a[a.length - 1] == '/') ? '' : '/') + b, callCount)
-        }
-
-        function callCount(err) {
-            if (err) console.log(err);
-            callTime--;
-            if (callTime == 0)
-                listDirectory(a, (files, fType) => {
-                    win.webContents.send('sendDirContent', files, fType)
-                })
-        }
-    });
-
-    ipcMain.on('doDelete', (event, files) => {
-        let callTime = files.length;
-        for (let file of files)
-            fs.remove(file, callCount);
-
-        function callCount(err) {
-            if (err) console.log(err);
-            callTime--;
-            if (callTime == 0)
-                listDirectory(user.path, (files, fType) => {
-                    win.webContents.send('sendDirContent', files, fType)
-                })
-        }
-    });
-
-    ipcMain.on('getPluginList', (event) => {
-        listDirectory('./plugin/script', (files) => {
-            let callTime = files.length;
-            let dir = './plugin/script/',
-                scripts = []
-            for (let file of files)
-                fs.readFile(dir + file, 'utf8', (err, data) => {
-                    if (err) { callCount(err); return; }
-                    scripts.push(data);
-                    callCount();
-                });
-
-            function callCount(err) {
-                if (err) console.log(err);
-                callTime--;
-                if (callTime == 0) {
-                    win.webContents.send('sendPluginList', scripts);
-                    console.log('Plugin loaded')
-                }
-            }
-        });
-    });
-
-    ipcMain.on('window', (event, command) => {
-        switch (command) {
-            case 'close':
-                try { win.close(); } catch (err) {};
-                break;
-        }
-    });
-    //#endregion
+function doneInit() {
+    win.webContents.send('sendCurrentDir', path.resolve(user.path).replace(/\\/g, '/'));
 }
+//#endregion
+
+//#region listener
+ipcMain.on('getDirContent', (event, path) => {
+    listDirectory(path, (files, fType) => {
+        win.webContents.send('sendDirContent', files, fType)
+    })
+    user.path = path;
+    fs.writeFile('./user/lastSessionPath', user.path, (err) => {
+        if (err) console.log(err)
+    })
+});
+
+ipcMain.on('getFileContent', (event, file) => {
+    file = path.resolve(user.path, file); // file is relative
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            win.webContents.send('sendFileContent', err);
+        } else
+            win.webContents.send('sendFileContent', data);
+    })
+});
+
+ipcMain.on('doSaveFile', (event, arr) => {
+    let file = arr[0];
+    let data = arr[1];
+    file = path.resolve(user.path, file); // file is relative
+    fs.writeFile(file, data, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+            win.webContents.send('didSaveFile', err);
+        }
+    });
+});
+
+ipcMain.on('doCopy', (event, files) => {
+    let callTime = files.length;
+    for (let file of files) {
+        let nDir = path.resolve(user.path, path.basename(file));
+        if (fs.lstatSync(file).isDirectory()) {
+            fs.ensureDirSync(nDir);
+            fs.copy(file, nDir, callCount);
+        } else {
+            fs.writeFile(nDir, '', (err) => {
+                if (err) return;
+                fs.copy(file, nDir, callCount)
+            })
+        }
+    }
+
+    function callCount(err) {
+        if (err) console.log(err);
+        callTime--;
+        if (callTime == 0)
+            listDirectory(user.path, (files, fType) => {
+                win.webContents.send('sendDirContent', files, fType)
+            })
+    }
+});
+
+ipcMain.on('doMove', (event, files) => {
+    let a = user.path;
+    let callTime = files.length;
+    for (let file of files) {
+        let b = file.split(`/`);
+        b = b[b.length - 1];
+        fs.rename(file, a + ((a[a.length - 1] == '/') ? '' : '/') + b, callCount)
+    }
+
+    function callCount(err) {
+        if (err) console.log(err);
+        callTime--;
+        if (callTime == 0)
+            listDirectory(a, (files, fType) => {
+                win.webContents.send('sendDirContent', files, fType)
+            })
+    }
+});
+
+ipcMain.on('doDelete', (event, files) => {
+    let callTime = files.length;
+    for (let file of files)
+        fs.remove(file, callCount);
+
+    function callCount(err) {
+        if (err) console.log(err);
+        callTime--;
+        if (callTime == 0)
+            listDirectory(user.path, (files, fType) => {
+                win.webContents.send('sendDirContent', files, fType)
+            })
+    }
+});
+
+ipcMain.on('getPluginList', (event) => {
+    listDirectory('./plugin/script', (files) => {
+        let callTime = files.length;
+        let dir = './plugin/script/',
+            scripts = []
+        for (let file of files)
+            fs.readFile(dir + file, 'utf8', (err, data) => {
+                if (err) { callCount(err); return; }
+                scripts.push(data);
+                callCount();
+            });
+
+        function callCount(err) {
+            if (err) console.log(err);
+            callTime--;
+            if (callTime == 0) {
+                win.webContents.send('sendPluginList', scripts);
+                console.log('Plugin loaded')
+            }
+        }
+    });
+});
+
+ipcMain.on('window', (event, command) => {
+    switch (command) {
+        case 'close':
+            try { win.close(); } catch (err) {};
+            break;
+    }
+});
+//#endregion
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin')
